8a0f98ec46f91b8fabcc58e8c2280fce81b8f564
AustinBoydston <austin.t.boydston-1@ou.edu>
2020-04-13 21:31:18 -0500

    added a to do list and some guide to the first layout

--- game.js
+++ game.js
@@ -1,2 +1,38 @@
-console.log('hello world');
-window.open();
+//Todo list
+/* 
+    -Create a structure that reads in questions and possible answers from a json file
+    -Have randomizing capabilities to the question calls
+    -Create the GUI elements for the question(text field), and four answer options (buttons)
+    -Display a message depending on right or wrong response
+    -Create a scoring mechanism for right and wrong answers 
+
+depending on how easy or hard the implementation, this script may be referenced by the html file or it may have it's contents copied and pasted directly into the html.
+
+
+basic layout for first prototype attempt:
+     ___________________________________
+    |                                   |    
+    |   Question: this is the question. |  <-- textfield
+    |                                   |
+    |                                   |
+    |                                   |
+    |-----------------------------------|
+    |   Answer 1        Answer 2        |  <-- buttons
+    |                                   |
+    |   Answer 3        Answer 4        |  <-- buttons
+    |___________________________________|
+
+
+
+
+*/
+
+//The canvas we have set up in the html file is called ctx
+// this ctx variable is linked to it
+var ctx = document.getElementById("ctx").getContext("2d");
+ctx.font = '30px Arial';
+
+//
+
+
+
